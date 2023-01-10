10c5006cdb7ee2f1c5a0945fd65ae64e804560bb
himesh <himeshkashyap347@gmail.com>
2023-01-10 13:10:16 -0800

    task 7 api call

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 function App() {
   const [movies ,setmovies]=useState([]);
-  const fetchMovieHAndler =()=>{
-    fetch('https://swapi.dev/api/films',).then((response)=>{
-      return response.json();
-    }).then((data)=>{
+  const [ isLoading,setLoading]=useState(false);
+
+  const  fetchMovieHAndler = async()=>{
+    setLoading(true);
+    const response=await fetch('https://swapi.dev/api/films',);
+    const data= await response.json();
       const transformedData=data.results.map(movieData=>{
         return {
           id:movieData.episode_id,
@@ -18,16 +20,18 @@ function App() {
         }
       })
       setmovies(transformedData);
-    })
+      setLoading(false);
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMovieHAndler}>Fetch Movies</button>
+        <button onClick={fetchMovieHAndler}  >Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length===0 && <p>No data found</p>}
+        {isLoading &&  <p>...Ladding</p>}
       </section>
     </React.Fragment>
   );
